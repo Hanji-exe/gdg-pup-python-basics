2f8cdc5724a8928ef2a652bf7bca3b0ab2e7acbe
hanji-exe <h.esmaelfahad@gmail.com>
2025-04-06 16:04:10 +0800

    Python_Dictionary
    About adding, updating and removing attributes in a  created dictionary

---
+++ Hanji_Assignments/Python_6/main.py
@@ -0,0 +1,12 @@
+
+myDict = { 'name': 'Hanji', 'age': 25}
+print("Main Dictionary:", myDict)
+
+myDict['City'] = 'Manila City'
+print('ADD: Updated Dictionary:', myDict)
+
+myDict['age'] = '19'
+print('UPDATE: Updated Dictionary:', myDict)
+
+del myDict['age']
+print('REMOVE: Updated Dictionary:', myDict)
