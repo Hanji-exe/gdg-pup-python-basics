56372dde38fe6068604a6248bfa974995ecd3fa9
hanji-exe <h.esmaelfahad@gmail.com>
2025-04-06 16:03:12 +0800

    Python_List
    About append, remove and sort method pycode

---
+++ Hanji_Assignments/Python_5/main.py
@@ -0,0 +1,19 @@
+#My List of integers
+myList =  [ 3 , 6 ,1 ]
+print(myList)
+
+#I used the append method to add an integer to the list
+myList.append(5)
+myList.append(2)
+myList.append(4)
+print(myList)
+
+#I used remove method for ano lang tinaggal ko ung odd number
+myList.remove(1)
+myList.remove(3)
+myList.remove(5)
+print(myList)
+
+#Using sort method to arrange the list in ascending order
+myList.sort()
+print(myList)
